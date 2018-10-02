653494864575340cef7a487dec44084c31d7428e
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2018-10-02 09:43:03 +0200

    mvc2 validation

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/Controller/ItemController.php';
+
+use Controller\ItemController;
+
+$items = new ItemController();
+$items->index();
+
+
 ?>
--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -1,21 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilder20
- * Date: 01/10/18
- * Time: 15:00
- */
+namespace Model;
 
 // src/Model/ItemManager.php
 require __DIR__ . '/../../app/db.php';
 
+
 // récupération de tous les items
-function selectAllItems() :array
+
+class ItemManager
 {
-    $pdo = new \PDO(DSN, USER, PASS);
-    $query = "SELECT * FROM item";
-    $res = $pdo->query($query);
-    return $res->fetchAll();
+    public function selectAllItems() :array
+    {
+        $pdo = new \PDO(DSN, USER, PASS);
+        $query = "SELECT * FROM item";
+        $res = $pdo->query($query);
+        return $res->fetchAll();
+    }
+    
 }
 
 ?>
